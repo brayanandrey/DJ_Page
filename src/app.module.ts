d5faf560f27aff8a9f82534c8d9dddd07a834427
Outsider <brayanandrey.torres@utp.edu.co>
2026-07-01 23:27:04 -0500

    Segunda versión de la estructura del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReleasesModule } from './modules/releases/releases.module';
+import { EventsModule } from './modules/events/events.module';
+import { ArtistsModule } from './modules/artists/artists.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ReleasesModule, EventsModule, ArtistsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
